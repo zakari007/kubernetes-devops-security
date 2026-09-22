58c87c86dc639d40048b76a8c138681071140f83
Zakari Abid <zakari.abid@gmail.com>
2026-09-22 16:04:21 +0300

    fix-bug

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -19,7 +19,7 @@ public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final String baseURL = "http://node-service:5000/plusone";
-	//private static final String baseURL = "http://localhost:5000/plusone";
+ // private static final String baseURL = "http://localhost:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
